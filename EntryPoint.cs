d342fa908ca4ef3c37a43a5ab3eb64127c297b73
KindRedSand <sand25g@gmail.com>
2022-10-01 02:23:15 +0300

    Fix Path error and fix Assemlies load pipeline

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Threading;
+
 using Razorwing.Framework.IO.Stores;
 using Razorwing.Framework.Logging;
 using Razorwing.Framework.Platform;
+
+using ReLogic.OS;
+
 using Terraria.ModKit.Razorwing.Overrides;
 
 namespace Terraria.ModKit
@@ -25,43 +30,7 @@ namespace Terraria.ModKit
                 Logger.Level = LogLevel.Debug;
 
                 Logger.Log("Start pre loading assemblies..");
-                AppDomain.CurrentDomain.AssemblyResolve += delegate (object sender, ResolveEventArgs sargs)
-                {
-                    bool mono = false;
-                    string resourceName = new AssemblyName(sargs.Name).Name + ".dll";
-                    string textx = Array.Find<string>(typeof(Program).Assembly.GetManifestResourceNames(), (string element) => element.EndsWith(resourceName));
-                    if (textx == null)
-                    {
-                        textx = Array.Find<string>(typeof(EntryPoint).Assembly.GetManifestResourceNames(), (string element) => element.EndsWith(resourceName));
-                        if (textx == null)
-                        {
-                            return null;
-                        }
-
-                        mono = true;
-                    }
-                    Assembly resultt;
-                    if (!mono)
-                    {
-                        using (Stream manifestResourceStream = typeof(Program).Assembly.GetManifestResourceStream(textx))
-                        {
-                            byte[] array = new byte[manifestResourceStream.Length];
-                            manifestResourceStream.Read(array, 0, array.Length);
-                            resultt = Assembly.Load(array);
-                        }
-                    }
-                    else
-                    {
-                        using (Stream manifestResourceStream = typeof(EntryPoint).Assembly.GetManifestResourceStream(textx))
-                        {
-                            byte[] array = new byte[manifestResourceStream.Length];
-                            manifestResourceStream.Read(array, 0, array.Length);
-                            resultt = Assembly.Load(array);
-                        }
-                    }
-                    Logger.Log($"Loaded {resultt.FullName}");
-                    return resultt;
-                };
+                AppDomain.CurrentDomain.AssemblyResolve += loaderdelegate;
                 Logger.Log("Finished assemblies pre loading loading");
 
 
@@ -77,11 +46,17 @@ namespace Terraria.ModKit
                     //result = Assembly.Load(array); 
                 }
                 //Newtonsoft.Json.JsonConvert.SerializeObject()
-                var s = (string)Reflect.InvokeS(result, "Newtonsoft.Json.JsonConvert", "SerializeObject", new object[] { new List<string>{"Terraria.ModKit", "v0.7.0"}});
+                var s = (string)Reflect.InvokeS(result, "Newtonsoft.Json.JsonConvert", "SerializeObject", new object[] { new List<string>{"Terraria.ModKit", "v0.7.1"}});
                 Console.WriteLine(s);
                 Logger.Log("Done! Registering initializer...");
                 //Program.LaunchGame(args);
+
+                Program.LaunchParameters = Utils.ParseArguements(args);
+                Program.SavePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "My Games", "Terraria");
+
                 Terraria.Main.OnEngineLoad += Entry.Initialise;
+
+
                 Logger.Log("Running game...");
                 Reflect.Invoke(typeof(WindowsLaunch), "Main", BindingFlags.Static | BindingFlags.NonPublic,
                     args);
@@ -91,6 +66,44 @@ namespace Terraria.ModKit
                 Console.WriteLine(e);
                 Console.ReadKey();
             }
+        }
+
+        private static Assembly loaderdelegate(object sender, ResolveEventArgs sargs)
+        {
+            bool mono = false;
+            string resourceName = new AssemblyName(sargs.Name).Name + ".dll";
+            string textx = Array.Find<string>(typeof(Program).Assembly.GetManifestResourceNames(), (string element) => element.EndsWith(resourceName));
+            if (textx == null)
+            {
+                textx = Array.Find<string>(typeof(EntryPoint).Assembly.GetManifestResourceNames(), (string element) => element.EndsWith(resourceName));
+                if (textx == null)
+                {
+                    return null;
+                }
+
+                mono = true;
+            }
+            Assembly resultt;
+            if (!mono)
+            {
+                using (Stream manifestResourceStream = typeof(Program).Assembly.GetManifestResourceStream(textx))
+                {
+                    byte[] array = new byte[manifestResourceStream.Length];
+                    manifestResourceStream.Read(array, 0, array.Length);
+                    resultt = Assembly.Load(array);
+                }
+            }
+            else
+            {
+                using (Stream manifestResourceStream = typeof(EntryPoint).Assembly.GetManifestResourceStream(textx))
+                {
+                    byte[] array = new byte[manifestResourceStream.Length];
+                    manifestResourceStream.Read(array, 0, array.Length);
+                    resultt = Assembly.Load(array);
+                }
+            }
+            Logger.Log($"Loaded {resultt.FullName}");
+            return resultt;
         }
     }
 }
